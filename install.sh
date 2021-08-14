24f8c28d87040f9057a0703f69a1d266cd5532c3
almeida-matheus <matheusalmeidacosta1212@gmail.com>
2021-08-14 20:15:12 -0300

    add new install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
-[ ! -x "$(which python)" ] && {
+pathscript="/opt/scripts" 
+pathlink="/usr/local/bin"
+
+check() {
+[ ! -x "$(which python3)" ] && {
     echo "python is not installed, please install it."
     exit 1
 }
@@ -9,44 +13,80 @@ if [ $(id -u) != 0 ]; then
     echo "permission denied, try again using sudo"
     exit 1
 fi
+}
 
-pathscript="/opt/scripts" 
-pathlink="/usr/local/bin"
-
+ask() {
+echo ""
 echo "========================================="
 echo "               install nam               "
 echo "========================================="
-echo "set default language"
+echo "set commands default language"
 echo "[1] - br"
 echo "[2] - en"
 echo -n "selected option: "
 read option;
 echo -e ""
+return "$option"
+}
 
-sudo cp -r ../nam $pathscript/ &
-sudo rm -rf $pathscript/nam/assets &
-sudo rm -rf $pathscript/nam/install.sh &
-sudo rm -rf $pathscript/nam/README.md &
+banner() {
+echo "         _  _   _   __  __   "
+echo "        | \| | /_\ |  \/  |  "
+echo "        |    |/ _ \| |\/| |  "
+echo "        |_|\_/_/ \_\_|  |_|  "
+echo ""
+}
 
-if [ ! -e $pathscript ]; then # if directory doesn't exist
-	sudo mkdir $pathscript # create directory
-fi
-sudo ln -sf $pathscript/nam/nam.py $pathlink/nam & # create link
-sudo chmod +x $pathscript/nam/nam.py &
-sudo chmod +x $pathlink/nam &
+prepare() {
+if [ ! -e $pathscript ]; then sudo mkdir $pathscript; fi
+if [ ! -e $pathscript/nam ]; then return; fi
+sudo rm -rf $pathscript/nam/commands/*
+}
 
-case $option in
+install(){
+sudo cp -r ../nam $pathscript/
+}
+
+link(){
+sudo ln -sf $pathscript/nam/nam.py $pathlink/nam 
+sudo chmod +x $pathscript/nam/nam.py 
+sudo chmod +x $pathlink/nam
+}
+
+language(){
+case $1 in
     "1")
-    # echo "brazilian portuguese version installed"
+    echo -e "commands: brazilian portuguese\n"
     ;;
     "2")
-    sudo cp -r $pathscript/nam/settings/language-br/commands/* $pathscript/nam/commands &
-    echo "sorry, english version wasn't released yet"
-    echo "brazilian portuguese version installed"
+    sudo cp -r $pathscript/nam/en/commands/* $pathscript/nam/commands 
+    echo -e "commands: english\n"
     ;;
     *)
-    echo "error: type only 1 or 2"
-    exit 1
+    echo -e "commands: brazilian portuguese\n"
 esac
+}
 
-echo "nam script was installed in: $pathscript"
+main(){
+check
+ask
+option=$?
+banner
+echo -ne '>>>>>>>>>                          [20%]\r'
+sleep 0.5
+echo -ne '>>>>>>>>>>>>>>>>                   [40%]\r'
+prepare
+sleep 0.5
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>            [60%]\r'
+install
+sleep 0.5
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>      [80%]\r'
+link
+sleep 0.5
+echo -ne '>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>[100%]\r'
+echo -ne '\n'
+echo -e "\nnam was installed in: $pathscript"
+language $option
+}
+
+main
